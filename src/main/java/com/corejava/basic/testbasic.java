9f95e190e3b1e488859eb48500904b18c1f24b29
amit-singh007 <amit.singh05@rocketmail.com>
2026-08-09 11:10:31 +0530

    init

---
+++ src/main/java/com/corejava/basic/testbasic.java
@@ -0,0 +1,9 @@
+package com.corejava.testbasic;
+
+import lombok.extern.log4j.Log4j2;
+@Log4j2
+public class testbasic {
+    static void main() {
+        log.info("First : Test Program display Here");
+    }
+}
---
+++ src/main/java/com/corejava/basics/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.corejava.basics;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class HelloWorld {
+    static void main() {
+        log.info("hello world");
+    }
+
+
+}
