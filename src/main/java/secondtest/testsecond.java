43f4694f49ad6e406489c765c6acf08e9e679e9e
amit-singh007 <amit.singh05@rocketmail.com>
2026-08-14 20:53:45 +0530

    init

---
+++ src/main/java/secondtest/testsecond.java
@@ -0,0 +1,10 @@
+package secondtest;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class testsecond {
+    static void main() {
+        log.info("Second program - Practice World");
+    }
+}
